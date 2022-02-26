52e80bfe040a206ce0fa427f28d31253e87e3d0c
Chris <93886036+ifmycodecouldtalk@users.noreply.github.com>
2022-02-26 00:07:52 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,36 +1,7 @@
-// Practicing with objects
-let user = {
-    name: "Chris",
-    age: 22,
-    job: "Software Developer",
+// alert, prompt, confirm
+let resultPrompt = prompt("How old are you?", 100);
+alert(`You are ${resultPrompt} years old`);
+let resultConfirm = confirm("This site will have lots of alerts I imagine");
 
-    introduction() {
-        console.log("My name is " + this.name + ', I am ' + this.age + ' years old. I am a ' + this.job + '.');
-    }
-};
-
-user.introduction();
-
-let users = [
-    { id: 1, name: "John" },
-    { id: 2, name: "Pete" },
-    { id: 3, name: "Mary" }
-];
-
-user = users.find(item => item.id == 1);
-
-// alert(user.name); // John
-
-users = [
-    { id: 1, name: "John" },
-    { id: 2, name: "Pete" },
-    { id: 3, name: "Mary" }
-];
-
-// returns array of the first two users
-let someUsers = users.filter(item => item.id < 3);
-
-// alert(someUsers.length); // 2
-
-let lengths = ["Bilbo", "Gandalf", "Nazgul"].map(item => item.length);
-alert(lengths); // 5,7,6
+let message = resultConfirm ? "Cool!" : "Bad!";
+alert(message);
